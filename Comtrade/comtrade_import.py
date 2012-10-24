d3728af1f22328c7f749e9be290a37349e30327e
Auggie <thomas-augspurger@uiowa.edu>
2012-10-24 15:07:27 -0500

    Changed parser to 'lxml' from 'xml' Starting the pull from Web.

--- Comtrade/comtrade_import.py
+++ Comtrade/comtrade_import.py
@@ -4,7 +4,7 @@ import lxml.html
 import urllib2
 from xml_parse import get_names
 
-soup = BeautifulSoup(open('Comtrade.xml'), 'xml')
+soup = BeautifulSoup(open('Comtrade.xml'), 'lxml')
 tag = soup.r
 
 # Refactor this to separate module.
@@ -25,6 +25,10 @@ df = pd.DataFrame(dictionary)
 
 ############ From the Web ############
 
-# file = lxml.html.parse(url)
-# url = 'http://comtrade.un.org/ws/get.aspx?px=H1&r=381&y=2003,2002&cc=TOTAL&p=0&comp=false'
+url = 'http://comtrade.un.org/ws/get.aspx?px=H1&r=381&y=2003,2002&cc=TOTAL&p=0&comp=false'
+file = lxml.html.parse(url)
 # or? html = urllib2.urlopen(url)
+
+access_code = '0+eQr1v/Ipy4z+k0Gbpgb7uR4roBFhizAELaCTsNqaRbSu5koYjlZSXNnj01N5Jq+qM/zN43pjkOtPV8EM1bi7p3JHZH7G221VQUoqJx0QO+LZ2QtYWVJ9tTgriP0b358hnSlo7DqWSizp4J1bIj6Mw9NQe5Pa85q6636s62AIA='
+xml = 'http://comtrade.un.org/ws/'
+xmlpath = 'getSdmxV1.aspx?cc=TOTAL&px=H2&r=372&y=2006&comp=false&code='
