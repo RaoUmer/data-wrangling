8c866c3a995bf7520598bc45094f9d4127cff632
Auggie <thomas-augspurger@uiowa.edu>
2012-10-24 15:54:18 -0500

    Refactoring DataFrame out of main import.

--- Comtrade/xml_parse.py
+++ Comtrade/xml_parse.py
@@ -1,4 +1,4 @@
-def get_names(tag):
+def get_names(tag, names):
     """
     Use to extract the column names from a element.Tag;
     
@@ -6,13 +6,41 @@ def get_names(tag):
     ----------
     
     * tag : an element.Tag (e.g. from BeautifulSoup)
+    * names : a list (probably empty; I was having trouble when names
+    was not a param, I think since it was declared locally and thus not
+    returned in the last line.  Maybe...
     
     Returns
     -------
     
-    * names : a list containing the columns to be used for a DataFrame.
+    * dictionary : a dict containing the columns to be used for a DataFrame
+    and initialized empty lists.
     """
     
-    names = []
     for i in range(len(tag.findChildren())):
-        return names.append(tag.findChildren()[i].name)
+        names.append(tag.findChildren()[i].name)
+    
+    values = [list([]) for _ in range(len(names))]
+    dictionary = dict(zip(names, values))
+    return dictionary
+
+def to_frame(soup, dictionary):
+    """
+    Next step in the data processing.
+    
+    Parameters
+    ----------
+    * soup : A BeautifulSoup object, parsed as xml, either from web or local.
+    * dictionary : A dict with index from columns and empty lists to be filled.
+    
+    Returns
+    -------
+    * Dataframe object with dict index -> column names. No unique index in df.
+    """
+    tags = soup.findAll('r')
+    for key in dictionary.keys():
+        for tag in tags:
+            dictionary[key].append(tag.find(key).string)
+
+    df = pd.DataFrame(dictionary)
+    
