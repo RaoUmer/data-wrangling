524439830cb9dbeffe163f09ca7cecb76a6b67e8
Auggie <thomas-augspurger@uiowa.edu>
2012-10-24 12:46:51 -0500

    fixed dictionary to be a dict, not list. Started work on import from database.

--- Comtrade/comtrade_import.py
+++ Comtrade/comtrade_import.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import pandas as pd
+import lxml.html
+import urllib2
 from xml_parse import get_names
 
 soup = BeautifulSoup(open('Comtrade.xml'), 'xml')
@@ -11,7 +13,7 @@ names = []
 for i in range(len(tag.findChildren())):
 names.append(tag.findChildren()[i].name)
 values = [list([]) for _ in range(len(names))]
-dictionary = zip(names, values)
+dictionary = dict(zip(names, values))
 
 tags = soup.findAll('r')
 
@@ -20,3 +22,9 @@ for key in dictionary.keys():
         dictionary[key].append(tag.find(key).string)
 
 df = pd.DataFrame(dictionary)
+
+############ From the Web ############
+
+# file = lxml.html.parse(url)
+# url = 'http://comtrade.un.org/ws/get.aspx?px=H1&r=381&y=2003,2002&cc=TOTAL&p=0&comp=false'
+# or? html = urllib2.urlopen(url)
