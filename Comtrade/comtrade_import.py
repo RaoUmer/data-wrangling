231e1f07b9eeb3c03a1f14b91a98513b68149a8d
Auggie <thomas-augspurger@uiowa.edu>
2012-10-25 11:13:23 -0500

    PEP8

--- Comtrade/comtrade_import.py
+++ Comtrade/comtrade_import.py
@@ -15,7 +15,9 @@ df = to_frame(soup, dictionary)
 
 xml = 'http://comtrade.un.org/ws/'
 xmlpath = 'get.aspx?cc=TOTAL&px=H2&r=372&y=2006&comp=false&code='
-access_code = '0+eQr1v/Ipy4z+k0Gbpgb7uR4roBFhizAELaCTsNqaRbSu5koYjlZSXNnj01N5Jq+qM/zN43pjkOtPV8EM1bi7p3JHZH7G221VQUoqJx0QO+LZ2QtYWVJ9tTgriP0b358hnSlo7DqWSizp4J1bIj6Mw9NQe5Pa85q6636s62AIA='
+access_code = '0+eQr1v/Ipy4z+k0Gbpgb7uR4roBFhizAELaCTsNqaRbSu5koYjlZSXNnj01'+\
+	'N5Jq+qM/zN43pjkOtPV8EM1bi7p3JHZH7G221VQUoqJx0QO+LZ2QtYWVJ9tTgriP0b358h'+\
+	'nSlo7DqWSizp4J1bIj6Mw9NQe5Pa85q6636s62AIA='
 
 soup = BeautifulSoup(urllib2.urlopen(
         xml + xmlpath + access_code), 'lxml')
