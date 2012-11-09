df03ec250d3cf9aff506fefb038d201f31b7e194
Auggie <thomas-augspurger@uiowa.edu>
2012-11-08 23:06:31 -0600

    Added year parse for YYYYxx.

--- Comext/year_month_parse.py
+++ Comext/year_month_parse.py
@@ -4,3 +4,8 @@ import datetime as dt
 def year_month(x):
     ym = (int(str(x)[:4]), int(str(x)[-2:]))
     return dt.datetime(ym[0], ym[1], 1)
+
+
+def year(x):
+    y = int(str(x)[:4])
+    return dt.datetime(y, 01, 1)
