0f22184e3dc37b6d5c719ed99f6d660a2a63ea72
Auggie <thomas-augspurger@uiowa.edu>
2012-11-12 19:49:07 -0600

    Documentation update.

--- Comext/gmm_calculation.py
+++ Comext/gmm_calculation.py
@@ -1,8 +1,19 @@
+import os
+from cPickle import load
+
+import numpy as np
 import pandas as pd
 from weighting_matrix import weight_matrix as wm
 
 
-def error():
+with open('years_dict.pkl', 'r') as years_dict:
+    years_dict = load(years_dict)
+years_dict.closed
+
+os.chdir('/Volumes/HDD/Users/tom/DataStorage/Comext/yearly')
+
+
+def error(store, country, good, partner, year, reference):
     """
     u_gct = [delta^k * ln(p_gct)]**2 - theta_1 * (delta^k * ln(s_gct))**2 -
         theta_2 * (delta^k * ln(p_gct) * delta^k * ln(s_gct))
@@ -10,7 +21,22 @@ def error():
     Where
         theta_1 = w_g / ((1 + w_g)(sigma_g - 1))
         theta_2 = (1 - w_g(sigma_g - 2)) / ((1 + w_g)(sigma_g -1))
+
+    To get shares: Something like
+
+    over total: df.xs(good, level='PRODUCT_NC')['VALUE_1000ECU'].sum()
+
+
     """
+
+    prices = (np.log(store[year + '_' + 'price' + '_' + country].xs(
+        (years_dict[year], good), level=('PERIOD', 'PRODUCT_NC'))) -
+        np.log(store[year + '_' + 'price' + '_' + reference].xs(
+        (years_dict[year], good), level=('PERIOD', 'PRODUCT_NC')))) ** 2
+
+    shares = (np.log(store[]))
+
+    # ASSUME A REFERENCE COUNTRY
 
 
 def get_reference(store, country,
@@ -28,7 +54,8 @@ def get_reference(store, country,
     TODO:
     Going to have to rework this to return a list of potentials for each good.
     From that list we'll (automatically according to some criteria) choose
-    the reference **for that good**.
+    the reference **for that good**.  That critera MUST include preference
+    for other countries in the dataset.  If not, no way to calculate moment.
 
     Parameters:
     -----------
