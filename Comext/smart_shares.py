2b093199fc37c8ebe9ee9400e72e135a66907528
Auggie <thomas-augspurger@uiowa.edu>
2012-11-21 07:20:04 -0600

    Used to generate gmm_store

--- Comext/smart_shares.py
+++ Comext/smart_shares.py
@@ -6,9 +6,11 @@ import pandas as pd
 from get_reference2 import get_reference
 
 os.chdir('/Volumes/HDD/Users/tom/DataStorage/Comext/yearly')
-yearly = pd.HDFStore('yearly.h5')
 
 # Globals
+
+yearly = pd.HDFStore('yearly.h5')
+gmm_store = pd.HDFStore('gmm_store.h5')
 
 with open('declarants_no_002_dict.pkl', 'r') as f:
     declarants = load(f)
@@ -16,9 +18,6 @@ f.closed
 
 years = [2007, 2008, 2009, 2010, 2011]
 
-for country in declarants:
-    for year in years[1:]:
-        gmm_store[year + country] = get_shares(country, year)
 
 def get_shares(country, year, store=yearly):
     """
@@ -67,77 +66,81 @@ def get_shares(country, year, store=yearly):
     return (
         np.log(df1 / gr1.sum().reindex(df1.index, level='PRODUCT_NC')).ix[iyear1] - (
         np.log(df0 / gr0.sum().reindex(df0.index, level='PRODUCT_NC')).ix[iyear0]) - (
-        np.log(df1.ix[l2].ix[iyear1].reset_index(level='PARTNER')['VALUE_1000ECU'].reindex(df1.index, level='PRODUCT_NC').ix[iyear1] / gr1.sum().reindex(df1.index, level='PRODUCT_NC').ix[iyear1]) - (
-        np.log(df0.ix[l4].ix[iyear0].reset_index(level='PARTNER')['VALUE_1000ECU'].reindex(df0.index, level='PRODUCT_NC').ix[iyear0] / gr0.sum().reindex(df0.index, level='PRODUCT_NC').ix[iyear0])
+        np.log(df1.ix[l1].ix[iyear1].reset_index(level='PARTNER')['VALUE_1000ECU'].reindex(df1.index, level='PRODUCT_NC').ix[iyear1] / gr1.sum().reindex(df1.index, level='PRODUCT_NC').ix[iyear1]) - (
+        np.log(df0.ix[l0].ix[iyear0].reset_index(level='PARTNER')['VALUE_1000ECU'].reindex(df0.index, level='PRODUCT_NC').ix[iyear0] / gr0.sum().reindex(df0.index, level='PRODUCT_NC').ix[iyear0])
         )
         )
-        )
+        ) ** 2
 
+    print('done with %r.') % country
+
+for country in declarants:
+    for year in years[1:]:
+        gmm_store['y' + str(year) + '_' + country] = get_shares(country, year)
 
 ##############################################################################
 # Testing
 
-os.chdir('/Volumes/HDD/Users/tom/DataStorage/Comext/yearly')
-yearly = pd.HDFStore('yearly.h5')
-country = '001'
-df07 = yearly['y2007_001'].head(500)['VALUE_1000ECU'].ix[1]
-df08 = yearly['y2008_001'].head(500)['VALUE_1000ECU'].ix[1]
+# os.chdir('/Volumes/HDD/Users/tom/DataStorage/Comext/yearly')
+# yearly = pd.HDFStore('yearly.h5')
+# country = '001'
+# df07 = yearly['y2007_001'].head(500)['VALUE_1000ECU'].ix[1]
+# df08 = yearly['y2008_001'].head(500)['VALUE_1000ECU'].ix[1]
 
-gr07 = df07.groupby(axis=0, level='PRODUCT_NC')
-gr08 = df08.groupby(axis=0, level='PRODUCT_NC')
+# gr07 = df07.groupby(axis=0, level='PRODUCT_NC')
+# gr08 = df08.groupby(axis=0, level='PRODUCT_NC')
 
-# Without Year
-# df = yearly['y2007_001'].head(500)['VALUE_1000ECU'].ix[1, 200752]
-# grouped = df.groupby(axis=0, level='PRODUCT_NC')
+# # Without Year
+# # df = yearly['y2007_001'].head(500)['VALUE_1000ECU'].ix[1, 200752]
+# # grouped = df.groupby(axis=0, level='PRODUCT_NC')
 
-ref_dict = get_reference(yearly, country)
-# Not necessary, good for testing though
-# ref_tuple = [(k, v) for k, v in ref_dict.iteritems()]
+# ref_dict = get_reference(yearly, country)
+# # Not necessary, good for testing though
+# # ref_tuple = [(k, v) for k, v in ref_dict.iteritems()]
 
-# Now Generalize to arbitrary product!
+# # Now Generalize to arbitrary product!
 
-# adiff = np.log(df08 / gr08.sum().reindex(df08.index, level='PRODUCT_NC')).ix[200852] - (
-#         np.log(df07 / gr07.sum().reindex(df07.index, level='PRODUCT_NC')).ix[200752]) - (
-#         Insert Arbitrary reference here)
+# # adiff = np.log(df08 / gr08.sum().reindex(df08.index, level='PRODUCT_NC')).ix[200852] - (
+# #         np.log(df07 / gr07.sum().reindex(df07.index, level='PRODUCT_NC')).ix[200752]) - (
+# #         Insert Arbitrary reference here)
 
-# Now diff with the reference
-# Pass .ix[] a list of tuples: get = [(200852, '01', 3), (200852, '01', 5)]
+# # Now diff with the reference
+# # Pass .ix[] a list of tuples: get = [(200852, '01', 3), (200852, '01', 5)]
 
-"""
-Let me tell you my strategy.
-We want the values for the ref country.  Start with the dict of pairs and
-filter out only those that are valid (i.e. there exists a reference partner).
+# """
+# Let me tell you my strategy.
+# We want the values for the ref country.  Start with the dict of pairs and
+# filter out only those that are valid (i.e. there exists a reference partner).
 
-Then filter down the full df to just the partners by passing a list of the
-(year, prodcut, ref_partner) tuples.
+# Then filter down the full df to just the partners by passing a list of the
+# (year, prodcut, ref_partner) tuples.
 
-Next drop the year and PARTNER levels with .ix[year] and rest_index & select
-just the Value.  This gives us a # Products x 1 series of the reference
-values.
+# Next drop the year and PARTNER levels with .ix[year] and rest_index & select
+# just the Value.  This gives us a # Produ2cts x 1 series of the reference
+# values.
 
-Now broadcast this up with .reindex.  Finally take the difference.
+# Now broadcast this up with .reindex.  Finally take the difference.
+# """
 
-"""
+# l1 = gr08.groups.keys()
+# l2 = []
+# drops08 = []
+# for x in l1:
+#     try:
+#         l2.append((200852, x, ref_dict[x]))
+#     except KeyError:
+#         drops08.append(x)
 
-l1 = gr08.groups.keys()
-l2 = []
-drops08 = []
-for x in l1:
-    try:
-        l2.append((200852, x, ref_dict[x]))
-    except KeyError:
-        drops08.append(x)
+# refs = df08.ix[l2]
 
-refs = df08.ix[l2]
-
-l3 = gr07.groups.keys()
-l4 = []
-drops07 = []
-for x in l3:
-    try:
-        l4.append((200752, x, ref_dict[x]))
-    except KeyError:
-        drops07.append(x)
+# l3 = gr07.groups.keys()
+# l4 = []
+# drops07 = []
+# for x in l3:
+#     try:
+#         l4.append((200752, x, ref_dict[x]))
+#     except KeyError:
+#         drops07.append(x)
 
 
 # ydiff = np.log(df08 / gr08.sum().reindex(df08.index, level='PRODUCT_NC')).ix[200852] - (
@@ -148,11 +151,11 @@ for x in l3:
 #         np.log(df07.ix[l4].ix[200752].reset_index(level='PARTNER')['VALUE_1000ECU'].reindex(df07.index, level='PRODUCT_NC').ix[200752] / gr07.sum().reindex(df07.index, level='PRODUCT_NC').ix[200752])
 #         )
 
-# ydiff - kdiff Goes in the table (squared)
+# # ydiff - kdiff Goes in the table (squared)
 
-np.log(df08 / gr08.sum().reindex(df08.index, level='PRODUCT_NC')).ix[200852] - (
-        np.log(df07 / gr07.sum().reindex(df07.index, level='PRODUCT_NC')).ix[200752]) - (
-        np.log(df08.ix[l2].ix[200852].reset_index(level='PARTNER')['VALUE_1000ECU'].reindex(df08.index, level='PRODUCT_NC').ix[200852] / gr08.sum().reindex(df08.index, level='PRODUCT_NC').ix[200852]) - (
-        np.log(df07.ix[l4].ix[200752].reset_index(level='PARTNER')['VALUE_1000ECU'].reindex(df07.index, level='PRODUCT_NC').ix[200752] / gr07.sum().reindex(df07.index, level='PRODUCT_NC').ix[200752])
-        )
-        )
+# np.log(df08 / gr08.sum().reindex(df08.index, level='PRODUCT_NC')).ix[200852] - (
+#         np.log(df07 / gr07.sum().reindex(df07.index, level='PRODUCT_NC')).ix[200752]) - (
+#         np.log(df08.ix[l2].ix[200852].reset_index(level='PARTNER')['VALUE_1000ECU'].reindex(df08.index, level='PRODUCT_NC').ix[200852] / gr08.sum().reindex(df08.index, level='PRODUCT_NC').ix[200852]) - (
+#         np.log(df07.ix[l4].ix[200752].reset_index(level='PARTNER')['VALUE_1000ECU'].reindex(df07.index, level='PRODUCT_NC').ix[200752] / gr07.sum().reindex(df07.index, level='PRODUCT_NC').ix[200752])
+#         )
+#         )
