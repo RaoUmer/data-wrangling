93b9c3bee8391e045fedd3222866976ca1c10ff4
Auggie <thomas-augspurger@uiowa.edu>
2012-11-22 07:50:45 -0600

    Should be final.

--- Comext/smart_cross.py
+++ Comext/smart_cross.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import pandas as pd
 from smart_shares import get_shares
 from smart_prices import get_prices
+from get_reference2 import get_reference
+
 
 start_time = datetime.now()
 os.chdir('/Volumes/HDD/Users/tom/DataStorage/Comext/yearly')
@@ -19,9 +21,11 @@ f.closed
 years = [2007, 2008, 2009, 2010, 2011]
 
 for country in sorted(declarants):
+    ref_dict = get_reference(yearly, country)
     for year in years[1:]:
         print 'Working on %r, %r.' % (country, year)
         print start_time - datetime.now()
-        gmm_store['c_' + country + '_y' + str(year)] = (
-            get_shares(country, year, square=1) * (
-            get_prices(country, year, square=1)))
+        gmm_store['s_' + country] = gmm_store['s_' + country].merge(
+            get_shares(country, year, square=1, name=('c_' + year)) *
+            get_prices(country, year, square=1, name=('c_' + year)),
+            how='outer', left_index=True, right_index=True)
