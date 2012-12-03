5e02b47969a423edecdf83ac7aeb1d8264030bff
Auggie <thomas-augspurger@uiowa.edu>
2012-12-03 10:47:40 -0600

    Nicer formatting with os.

--- Eurostat/trade_wedge/trade_wedge.py
+++ Eurostat/trade_wedge/trade_wedge.py
@@ -1,5 +1,7 @@
 from __future__ import division
 
+import os
+
 import pandas as pd
 import numpy as np
 from time_parsers import year_quarter
@@ -39,33 +41,33 @@ Demand
     http://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-055786_QID_-7AEE9C80_UID_-3F171EB0&layout=TIME,C,X,0;GEO,L,Y,0;S_ADJ,L,Z,0;UNIT,L,Z,1;INDIC_NA,L,Z,2;INDICATORS,C,Z,3;&zSelection=DS-055786INDICATORS,OBS_FLAG;DS-055786S_ADJ,NSA;DS-055786UNIT,MIO_EUR;DS-055786INDIC_NA,P3_P5;&rankName1=TIME_1_0_0_0&rankName2=INDIC-NA_1_2_-1_2&rankName3=S-ADJ_1_2_-1_2&rankName4=INDICATORS_1_2_-1_2&rankName5=UNIT_1_2_-1_2&rankName6=GEO_1_2_0_1&pprRK=FIRST&pprSO=PROTOCOL&ppcRK=FIRST&ppcSO=ASC&sortC=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=NONE&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23
 
 """
-
-gdp_c = pd.read_csv('/Users/tom/TradeData/data-wrangling/Eurostat/trade_wedge/chained_gdp.csv',
+os.chdir('/Users/tom/TradeData/data-wrangling/Eurostat/trade_wedge/')
+gdp_c = pd.read_csv('chained_gdp.csv',
     index_col=[1, 0], parse_dates=[1], date_parser=year_quarter, thousands=',',
     na_values=':').drop(['UNIT', 'INDIC_NA', 'S_ADJ'], axis=1)
 gdp_c = gdp_c.unstack().stack(1)
 
-gdp_n = pd.read_csv('/Users/tom/TradeData/data-wrangling/Eurostat/trade_wedge/nominal_gdp.csv', index_col=[1, 0],
+gdp_n = pd.read_csv('nominal_gdp.csv', index_col=[1, 0],
     parse_dates=[1], date_parser=year_quarter, thousands=',',
     na_values=':').drop(['UNIT', 'INDIC_NA', 'S_ADJ'], axis=1)
 gdp_n = gdp_n.unstack().stack(1)
 
-imports_c = pd.read_csv('/Users/tom/TradeData/data-wrangling/Eurostat/trade_wedge/chained_imports.csv', index_col=[0, 1],
+imports_c = pd.read_csv('chained_imports.csv', index_col=[0, 1],
     parse_dates=[0], date_parser=year_quarter, thousands=',',
     na_values=':').drop(['UNIT', 'INDIC_NA', 'S_ADJ'], axis=1)
 imports_c = imports_c.unstack().stack(1)
 
-imports_n = pd.read_csv('/Users/tom/TradeData/data-wrangling/Eurostat/trade_wedge/nominal_imports.csv', index_col=[0, 1],
+imports_n = pd.read_csv('nominal_imports.csv', index_col=[0, 1],
     parse_dates=[0], date_parser=year_quarter, thousands=',',
     na_values=':').drop(['UNIT', 'INDIC_NA', 'S_ADJ'], axis=1)
 imports_n = imports_n.unstack().stack(1)
 
-demand_c = pd.read_csv('/Users/tom/TradeData/data-wrangling/Eurostat/trade_wedge/chained_demand.csv', index_col=[0, 1],
+demand_c = pd.read_csv('chained_demand.csv', index_col=[0, 1],
     parse_dates=[0], date_parser=year_quarter, thousands=',',
     na_values=':').drop(['UNIT', 'INDIC_NA', 'S_ADJ'], axis=1)
 demand_c = demand_c.unstack().stack(1)
 
-demand_n = pd.read_csv('/Users/tom/TradeData/data-wrangling/Eurostat/trade_wedge/nominal_demand.csv', index_col=[0, 1],
+demand_n = pd.read_csv('nominal_demand.csv', index_col=[0, 1],
     parse_dates=[0], date_parser=year_quarter, thousands=',',
     na_values=':').drop(['UNIT', 'INDIC_NA', 'S_ADJ'], axis=1)
 demand_n = demand_n.unstack().stack(1)
