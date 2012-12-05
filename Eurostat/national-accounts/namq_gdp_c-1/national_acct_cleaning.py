7c208acfc418f7c2147a41ff3e2c4f3dc46d3d3d
Auggie <thomas-augspurger@uiowa.edu>
2012-12-05 14:45:40 -0600

    Added parser for thousands.  Correct sort level now. The index levels are swapped when I read.

--- Eurostat/national-accounts/namq_gdp_c-1/national_acct_cleaning.py
+++ Eurostat/national-accounts/namq_gdp_c-1/national_acct_cleaning.py
@@ -19,16 +19,16 @@ def time_parse(x):
 
 
 df = pd.read_csv('gdp.csv', na_values=":", parse_dates=[0], index_col=[0, 1],
-    date_parser=time_parse)
+    date_parser=time_parse, thousands=",")
 
 del df['S_ADJ'], df['UNIT'], df['INDIC_NA']
 df = df.rename(columns={'Value': 'gdp'})
 
 df2 = pd.read_csv('imports.csv', na_values=":", parse_dates=[0],
- index_col=[0, 1], date_parser=time_parse)
+ index_col=[0, 1], date_parser=time_parse, thousands=",")
 
 df3 = pd.read_csv('exports.csv', na_values=":", parse_dates=[0],
-    index_col=[0, 1], date_parser=time_parse)
+    index_col=[0, 1], date_parser=time_parse, thousands=",")
 
 del df2['S_ADJ'], df2['UNIT'], df2['INDIC_NA'], df3['S_ADJ'],
 del df3['UNIT'], df3['INDIC_NA']
@@ -54,6 +54,6 @@ for x in df.index:
 
 df.index = pd.MultiIndex.from_tuples(l)
 df.index.names = ['quarter', 'geo']
-df = df.sortlevel()
+df = df.sortlevel(1)
 
 df.to_csv('na_accts_clean.csv')
