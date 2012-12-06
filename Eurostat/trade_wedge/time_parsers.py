dfc8cf85e2245868ba35cb170608ce107e5a479a
Auggie <thomas-augspurger@uiowa.edu>
2012-12-05 23:57:57 -0600

    Alternative quarter parser.

--- Eurostat/trade_wedge/time_parsers.py
+++ Eurostat/trade_wedge/time_parsers.py
@@ -9,3 +9,18 @@ def year_month(x):
 def year_quarter(x):
     year, quarter = (int(x[:4]), int(x[-1]) * 3)
     return dt.datetime(year, quarter, 1)
+
+
+def quarter_datetime(x):
+    """convert '2012Q3' -> good format
+    """
+    q_to_m = {'1': 3,
+            '2': 6,
+            '3': 9,
+            '4': 12}
+    l = x.split('Q')
+    return dt.datetime(int(l[0]), q_to_m[l[1]], 1)
+
+
+if __name__ == '__main__':
+    pass
