f3e9346da5f2d477f8ed695177be0eb1d8d5fbb8
Auggie <thomas-augspurger@uiowa.edu>
2012-12-10 17:48:03 -0600

    Initial Commit for industrial production.

---
+++ Eurostat/ind_prod/ind_prod.py
@@ -0,0 +1,16 @@
+import os
+import datetime as dt
+
+import numpy as np
+import pandas as pd
+
+os.chdir('/Volumes/HDD/Users/tom/DataStorage/Eurostat/ind_prod_store/')
+df = pd.read_csv('ind_prod_clean.csv', index_col=['time', 'geo']))
+
+prod.index.levels[0] = pd.DatetimeIndex(prod.index.levels[0])
+
+"""
+Just some notes
+test = prod.xs('AT', level='geo')[['B', 'C', 'D']]
+test.ix['2007-03-01':'2012-09-01'].pct_change(4)
+"""
