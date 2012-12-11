b178fdb22ce6d5e9404ac5efeec3f8eb42542003
Auggie <thomas-augspurger@uiowa.edu>
2012-12-10 23:02:03 -0600

    Bug fixes.

--- Eurostat/ind_prod/ind_prod.py
+++ Eurostat/ind_prod/ind_prod.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 os.chdir('/Volumes/HDD/Users/tom/DataStorage/Eurostat/ind_prod_store/')
-df = pd.read_csv('ind_prod_clean.csv', index_col=['time', 'geo']))
+prod = pd.read_csv('ind_prod_clean.csv', index_col=['time', 'geo'])
 
 prod.index.levels[0] = pd.DatetimeIndex(prod.index.levels[0])
 
