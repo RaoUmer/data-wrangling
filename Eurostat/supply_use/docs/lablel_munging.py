61f8380cdc408811255b56a03d2b5addd16a4e27
Auggie <thomas-augspurger@uiowa.edu>
2012-12-11 13:54:38 -0600

    Typo; handle both at once. Correct names.

--- Eurostat/supply_use/docs/lablel_munging.py
+++ Eurostat/supply_use/docs/lablel_munging.py
@@ -1,19 +1,19 @@
 import cPickle
 
-files = ["use_col_lables.txt", "use_row_labels.txt"]
+files = ["use_col_labels.txt", "use_row_labels.txt"]
 
-name = files[1]
-f = open(name, 'r')
-lines = f.readlines()
-f.close()
+for name in files:
+    f = open(name, 'r')
+    lines = f.readlines()
+    f.close()
 
-sp = []
+    sp = []
 
-for l in lines:
-    sp.append(l.split(' ', 1))
+    for l in lines:
+        sp.append(l.split(' ', 1))
 
-d = {k: v.lstrip() for k, v in sp}
+    d = {k: v.lstrip() for k, v in sp}
 
-out = open('clean_' + name[:-4] + '.pkl', 'w')
-cPickle.dump(d, out, protocol=2)
-out.close()
+    out = open('clean_' + name[:-4] + '.pkl', 'w')
+    cPickle.dump(d, out, protocol=2)
+    out.close()
