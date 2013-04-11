72d31b527b2b52716a4e4739112b4913dd706881
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-04-11 10:34:46 -0500

    Probably not very relavent anymore.
    Crufty.
    
    Using for the final run.  Note, still no weighting,
    despite the title.
    
    Cleaning up comments

--- new_start/gmm_with_weighting.py
+++ new_start/gmm_with_weighting.py
@@ -1,8 +1,4 @@
 """
-# TODO: You last run (may) have failed because some of the optimizations
-will  return None.  That makes your dataframe of type ```object```,
-which cannot be appended.
-
 You have a df like:
                           'p', s, c
 year, partner, product ||
@@ -20,32 +16,6 @@ from scipy import dot
 import gmail
 from parse_optimize_results import opt_dict_format
 #-----------------------------------------------------------------------------
-
-
-def sse_w(x, c, p, s, W=None):
-    """
-    minimize this in the GMM.  Make sure you're
-    estimating the params of interst and not theta...
-
-    Parameters
-
-    * x: array of parameters to be argmined.
-    * c: the cross part from estimation equation.
-    * p: the price part from estimation equation.
-    * s: the share part from estimation equation.
-    * W: weighting matrix.  Default None -> I.
-
-    Returns
-
-    The sum of square errors, possibly weighted by W.
-    """
-    if W is None:
-        W = np.eye(len(c))
-    t1, t2 = x
-    # t1 = w / ((1 + w) * (sig - 1))
-    # t2 = (1 - w * (sig - 2)) / ((1 + w) * (sig - 1))
-    u = p ** 2 - t1 * s ** 2 - t2 * c
-    return (1 / len(c)) * dot(dot(u, W), u)
 
 
 def _theta_to_interest(t1, t2):
@@ -120,9 +90,15 @@ if __name__ == '__main__':
         for_csv, for_hd5 = opt_dict_format(res, names=['t1', 't2'])
         try:
             gmm_results.append('res_' + ctry, for_hd5)
-        except TypeError:
+        except:
+            with open(base + 'failed_h5.txt', 'a') as f:
+                f.write("Missed on {}".format(ctry))
+        try:
             for_csv.to_csv('/Volumes/HDD/Users/tom/DataStorage/Comext/'
                            'yearly/ctry_{}.csv'.format(ctry))
+        except:
+            with open(base + 'failed_csv.txt', 'a') as f:
+                f.write("Missed on {}".format(ctry))
 
         m = 'Finshed country {0} in {1}'.format(ctry, datetime.utcnow() - t)
         try:
--- new_start/weighting_perf.py
+++ new_start/weighting_perf.py
@@ -5,10 +5,13 @@ from cPickle import load
 import pandas as pd
 
 from gmm_with_weighting import gen_params
-
+#-----------------------------------------------------------------------------
+# Data IO
 base = '/Volumes/HDD/Users/tom/DataStorage/Comext/yearly/'
+
 gmm = pd.HDFStore(base + 'gmm_store.h5')
 gmm_results = pd.HDFStore(base + 'gmm_results.h5')
+
 with open(base + 'declarants_no_002_dict.pkl', 'r') as declarants:
     country_code = load(declarants)
 
@@ -16,11 +19,14 @@ ctry = '002'
 df = gmm.select('by_ctry_' + ctry)
 
 sub = df[:1000]
-sub_pp = sub.dropna().groupby(level=['PRODUCT_NC', 'PARTNER'])
+sub_p = sub.dropna().groupby(level='PRODUCT_NC')
 
+#-----------------------------------------------------------------------------
 # Python version
 res = {name: gen_params(group, x0=[2, 1]) for name, group
-       in by_product_partner}
+       in sub_p}
+# %timeit {name: gen_params(group, x0=[2, 1]) for name, group in sub_p}
+# 1 loops, best of 3: 1.98 s per loop
 
-# %timeit res = {name: gen_params(group, x0=[2, 1]) for name, group in sub_pp}
-# 1 loops, best of 3: 36.9 s per loop
+#-----------------------------------------------------------------------------
+# Cythonized
